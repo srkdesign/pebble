4bb141762836ac99924a16f8a67dc0bda4033468
srkdesign <99723068+srkdesign@users.noreply.github.com>
2026-07-29 08:24:53 +0400

    Added slashes to urls

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "pebble",
+  base: "/pebble/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
